ebd50ca38537668df2d76e784fc940fbd33ac091
Anton Isaev <jcant.gm@gmail.com>
2021-09-30 12:58:10 +0300

    correct sql file

--- base_init.sql
+++ base_init.sql
@@ -4,6 +4,10 @@ CREATE DATABASE JLinkManagement;
 
 GRANT ALL PRIVILEGES ON JLinkManagement.* TO jlink_admin@localhost;
 
+
+--
+-- Table structure for table `hibernate_sequence`
+--
 DROP TABLE IF EXISTS `hibernate_sequence`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -12,10 +16,6 @@ CREATE TABLE `hibernate_sequence` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `hibernate_sequence`
---
-
 LOCK TABLES `hibernate_sequence` WRITE;
 /*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
 INSERT INTO `hibernate_sequence` VALUES (27);
@@ -23,9 +23,33 @@ INSERT INTO `hibernate_sequence` VALUES (27);
 UNLOCK TABLES;
 
 --
+-- Table structure for table `juser`
+--
+DROP TABLE IF EXISTS `juser`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `juser` (
+  `id` bigint NOT NULL,
+  `blocked` bit(1) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `login` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `reset_password` bit(1) NOT NULL,
+  `role` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+LOCK TABLES `juser` WRITE;
+/*!40000 ALTER TABLE `juser` DISABLE KEYS */;
+INSERT INTO `juser` VALUES (1,_binary '\0',NULL,'admin',NULL,'$2a$10$mvuMNa9iOkNJK1LyWLPj9uh.xaICWGjC78iRZkkdF9auHDjZLbjx.',_binary '\0','ADMIN'),(2,_binary '\0',NULL,'user',NULL,'$2a$10$mvuMNa9iOkNJK1LyWLPj9uh.xaICWGjC78iRZkkdF9auHDjZLbjx.',_binary '\0','USER');
+/*!40000 ALTER TABLE `juser` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `jadvertising`
 --
-
 DROP TABLE IF EXISTS `jadvertising`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -40,10 +64,6 @@ CREATE TABLE `jadvertising` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `jadvertising`
---
-
 LOCK TABLES `jadvertising` WRITE;
 /*!40000 ALTER TABLE `jadvertising` DISABLE KEYS */;
 INSERT INTO `jadvertising` VALUES 
@@ -55,7 +75,6 @@ UNLOCK TABLES;
 --
 -- Table structure for table `jarticle`
 --
-
 DROP TABLE IF EXISTS `jarticle`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -73,10 +92,6 @@ CREATE TABLE `jarticle` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `jarticle`
---
-
 LOCK TABLES `jarticle` WRITE;
 /*!40000 ALTER TABLE `jarticle` DISABLE KEYS */;
 INSERT INTO `jarticle` VALUES 
@@ -90,7 +105,6 @@ UNLOCK TABLES;
 --
 -- Table structure for table `jlink`
 --
-
 DROP TABLE IF EXISTS `jlink`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -109,10 +123,6 @@ CREATE TABLE `jlink` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `jlink`
---
-
 LOCK TABLES `jlink` WRITE;
 /*!40000 ALTER TABLE `jlink` DISABLE KEYS */;
 INSERT INTO `jlink` VALUES (11,_binary '','2021-11-30 00:00:01.707000',_binary '','2021-09-29 00:00:01.698000','google.com/search?q=java+spring','short1.jca/qqwwee',1),(12,_binary '','2021-11-30 00:00:01.707000',_binary '\0','2021-09-29 00:00:01.698000','www.linkedin.com/in/anton-isaev','short1.jca/adminlink',1),(13,_binary '','2021-11-30 00:00:01.707000',_binary '\0','2021-09-29 00:00:01.698000','github.com/jcant','owesome.short1.jca',1),(14,_binary '','2021-11-30 00:00:01.707000',_binary '','2021-09-29 00:00:01.698000','google.com/search?q=oop+principles','short2.jca/aassdd',2),(15,_binary '','2021-11-30 00:00:01.707000',_binary '\0','2021-09-29 00:00:01.698000','github.com/jcant','short2.jca/userlink',2),(16,_binary '','2021-11-30 00:00:01.707000',_binary '\0','2021-09-29 00:00:01.698000','www.linkedin.com/in/anton-isaev','super.short2.jca',2);
@@ -122,7 +132,6 @@ UNLOCK TABLES;
 --
 -- Table structure for table `jlink_click`
 --
-
 DROP TABLE IF EXISTS `jlink_click`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -137,10 +146,6 @@ CREATE TABLE `jlink_click` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `jlink_click`
---
-
 LOCK TABLES `jlink_click` WRITE;
 /*!40000 ALTER TABLE `jlink_click` DISABLE KEYS */;
 INSERT INTO `jlink_click` VALUES (18,'127.0.0.1','2021-09-30 10:36:47.280000',11),(19,'127.0.0.1','2021-09-30 10:37:13.023000',12),(20,'127.0.0.1','2021-09-30 10:37:41.127000',13),(26,'127.0.0.1','2021-09-30 11:32:53.283000',11);
@@ -150,7 +155,6 @@ UNLOCK TABLES;
 --
 -- Table structure for table `jroot_link`
 --
-
 DROP TABLE IF EXISTS `jroot_link`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -163,50 +167,16 @@ CREATE TABLE `jroot_link` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `jroot_link`
---
-
 LOCK TABLES `jroot_link` WRITE;
 /*!40000 ALTER TABLE `jroot_link` DISABLE KEYS */;
 INSERT INTO `jroot_link` VALUES (9,_binary '','short1.jca'),(10,_binary '\0','short2.jca');
 /*!40000 ALTER TABLE `jroot_link` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `juser`
---
-
-DROP TABLE IF EXISTS `juser`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `juser` (
-  `id` bigint NOT NULL,
-  `blocked` bit(1) NOT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `login` varchar(255) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `reset_password` bit(1) NOT NULL,
-  `role` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `juser`
---
-
-LOCK TABLES `juser` WRITE;
-/*!40000 ALTER TABLE `juser` DISABLE KEYS */;
-INSERT INTO `juser` VALUES (1,_binary '\0',NULL,'admin',NULL,'$2a$10$mvuMNa9iOkNJK1LyWLPj9uh.xaICWGjC78iRZkkdF9auHDjZLbjx.',_binary '\0','ADMIN'),(2,_binary '\0',NULL,'user',NULL,'$2a$10$mvuMNa9iOkNJK1LyWLPj9uh.xaICWGjC78iRZkkdF9auHDjZLbjx.',_binary '\0','USER');
-/*!40000 ALTER TABLE `juser` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `juser_log`
 --
-
 DROP TABLE IF EXISTS `juser_log`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -221,10 +191,6 @@ CREATE TABLE `juser_log` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `juser_log`
---
-
 LOCK TABLES `juser_log` WRITE;
 /*!40000 ALTER TABLE `juser_log` DISABLE KEYS */;
 INSERT INTO `juser_log` VALUES (17,'2021-09-30 10:34:43.725000','127.0.0.1',1),(21,'2021-09-30 10:56:11.346000','127.0.0.1',1),(22,'2021-09-30 11:07:44.267000','127.0.0.1',1),(23,'2021-09-30 11:14:50.680000','127.0.0.1',1),(24,'2021-09-30 11:30:18.565000','127.0.0.1',1),(25,'2021-09-30 11:31:21.590000','127.0.0.1',1);
@@ -234,7 +200,6 @@ UNLOCK TABLES;
 --
 -- Table structure for table `juser_payments_log`
 --
-
 DROP TABLE IF EXISTS `juser_payments_log`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -253,10 +218,6 @@ CREATE TABLE `juser_payments_log` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `juser_payments_log`
---
-
 LOCK TABLES `juser_payments_log` WRITE;
 /*!40000 ALTER TABLE `juser_payments_log` DISABLE KEYS */;
 /*!40000 ALTER TABLE `juser_payments_log` ENABLE KEYS */;
@@ -270,5 +231,3 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-09-30 11:50:53
